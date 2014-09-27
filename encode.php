902abd2ad9403e4d9d2fee8a89f6da9f1ec453f0
Alexandre Girard <git@alexgirard.com>
2014-09-27 14:15:14 +0200

    try parameter to avoid overlay issue

--- encode.php
+++ encode.php
@@ -306,7 +306,7 @@ try {
 
     # Add overlay : http://stackoverflow.com/a/10920872
     $overlay = dirname(__FILE__) . "/watermark/watermark.png";
-    $overlay_command = $ffmpeg_path . ' -y -i ' .  escapeshellarg($flv) . ' -i ' . $overlay . ' -filter_complex "overlay=main_w/2-overlay_w/2:main_h/2-overlay_h/2" -codec:a copy ' . escapeshellarg($flv) . ' >> ' . escapeshellarg($debug_log) . ' 2>&1';
+    $overlay_command = $ffmpeg_path . ' -y -i ' .  escapeshellarg($flv) . ' -i ' . $overlay . ' -b:v 64k -filter_complex "overlay=main_w/2-overlay_w/2:main_h/2-overlay_h/2" -codec:a copy ' . escapeshellarg($flv) . ' >> ' . escapeshellarg($debug_log) . ' 2>&1';
 
     // Debug Log
     $log_msg = "\n\n\n\n==================================================================\n";
